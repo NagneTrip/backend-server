c6421ae3a6113b927258a8d0857d1da3cf389e17
enduf7686 <enduf768640@gmail.com>
2024-04-19 23:50:19 +0900

    initial commit

---
+++ src/main/java/com/ssafy/nagne/NagneApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.nagne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NagneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/nagne/NagneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.nagne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
