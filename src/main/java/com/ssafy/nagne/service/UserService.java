43da0973233daa725da8624306e9ed6ac254fd56
enduf7686 <enduf768640@gmail.com>
2024-04-22 01:59:59 +0900

    feat: UserService 추가
    - login() 구현

---
+++ src/main/java/com/ssafy/nagne/service/UserService.java
@@ -0,0 +1,48 @@
+package com.ssafy.nagne.service;
+
+import static lombok.Lombok.checkNotNull;
+
+import com.ssafy.nagne.entity.User;
+import com.ssafy.nagne.error.NotFoundException;
+import com.ssafy.nagne.repository.UserRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public User login(String username, String password) {
+        User user = findByUsername(username)
+                .orElseThrow(() -> new NotFoundException("Could not fount user for " + username));
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new IllegalArgumentException("Bad Credential");
+        }
+
+        user.afterLogin();
+        userRepository.update(user);
+
+        return user;
+    }
+
+    public Optional<User> findById(Long id) {
+        checkNotNull(id, "id must be provided");
+
+        return userRepository.findById(id);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        checkNotNull(username, "username must be provided");
+
+        return userRepository.findByUsername(username);
+    }
+}
